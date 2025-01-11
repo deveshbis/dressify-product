29a06d32e97bb9c1fb63d57941c82c84bcc6a2f6
deveshbis <deveshbiswas.cse@gmail.com>
2025-01-11 22:47:42 +0600

    update banner section

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,43 @@
 <template>
   <div>
-    <Banner />
+    <!-- <Banner /> -->
+    <section
+      class="relative bg-[url(https://www.smarttech-tv.com/image/cache/catalog/home_banner/HomeBanner_1920x960_monitor-1920x960.jpg)] bg-cover bg-center bg-no-repeat">
+      <div
+        class="absolute inset-0 bg-gray-900/75 sm:bg-transparent sm:from-gray-900/95 sm:to-gray-900/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"></div>
+
+      <div
+        class="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
+        <div class="max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
+          <h1 class="text-3xl font-extrabold text-white sm:text-5xl">
+            Let us find your
+
+            <strong class="block font-extrabold text-rose-500">
+              Forever Product.
+            </strong>
+          </h1>
+
+          <p class="mt-4 max-w-lg text-white sm:text-xl/relaxed">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt
+            illo tenetur fuga ducimus numquam ea!
+          </p>
+
+          <div class="mt-8 flex flex-wrap gap-4 text-center">
+            <a
+              href="#"
+              class="block w-full rounded bg-rose-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-rose-700 focus:outline-none focus:ring active:bg-rose-500 sm:w-auto">
+              Get Started
+            </a>
+
+            <a
+              href="#"
+              class="block w-full rounded bg-white px-12 py-3 text-sm font-medium text-rose-600 shadow hover:text-rose-700 focus:outline-none focus:ring active:text-rose-500 sm:w-auto">
+              Learn More
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
     <div class="font-sans p-4 mx-auto lg:max-w-7xl md:max-w-3xl">
       <div v-if="loading" class="flex justify-center items-center h-[60vh]">
         <div class="spinner-border animate-spin inline-block w-12 h-12 border-4 rounded-full text-blue-600" role="status">
